53ce5b38af661ac963a59e96bbf7e1f6d54bb114
Anthony <avcaliani.it@gmail.com>
2019-11-20 23:32:47 -0300

    Adding Reddit Data

---
+++ mr-owlf-dss/mr_owlf_dss/ingestors/reddit.py
@@ -0,0 +1,48 @@
+from os import path, getcwd
+from logging import getLogger
+
+import pandas as pd
+
+__author__ = 'Anthony Vilarim Caliani'
+__contact__ = 'https://github.com/avcaliani'
+__license__ = 'MIT'
+
+DATA_DIR = path.realpath(path.join(getcwd(), path.dirname(__file__), '../../data/reddit'))
+
+
+class Reddit:
+
+    def __init__(self):
+        self.log = getLogger('root')
+        self.log.debug(f'Data Directory: "{DATA_DIR}"')
+
+    def clean_data(self, posts):
+        self.log.debug(f'Data Shape (Raw): {posts.shape}')
+        posts.drop_duplicates(subset='title', inplace=True)             # Drop duplicate rows
+        posts['title'] = posts['title'].str.replace('[^\w\s]', ' ')     # Remove punctuation
+        posts['title'] = posts['title'].str.replace('[^A-Za-z]', ' ')   # Remove numbers
+        posts['title'] = posts['title'].str.replace('\s{2,}', ' ')      # Make sure any double-spaces are single
+        posts['title'] = posts['title'].str.lower()                     # Transform all text to lowercase
+        posts.dropna(inplace=True)                                      # Remove null records
+        self.log.debug(f'Data Shape (Cleaned): {posts.shape}')
+
+    def exec(self):
+
+        df_onion = pd.read_csv(f'{DATA_DIR}/the-onion.csv')
+        self.log.info(f'The Onion (Shape): {df_onion.shape}')
+
+        df_not_onion = pd.read_csv(f'{DATA_DIR}/not-the-onion.csv')
+        self.log.info(f'Not The Onion (Shape): {df_not_onion.shape}')
+
+        posts = pd.concat([df_onion, df_not_onion], axis=0)
+        self.log.info(f'Combined Shape: {posts.shape}')
+
+        posts = posts.reset_index(drop=True)
+        posts = posts.drop(columns=['n', 'num_comments', 'score'])
+        self.clean_data(posts)
+
+        # FIXME: POC Purpose
+        posts["subreddit"] = posts["subreddit"].map({"nottheonion": 0, "TheOnion": 1})
+        posts = posts.rename(columns={'subreddit': '__classification__'})
+
+        return posts
--- mr-owlf-dss/mr_owlf_dss/main.py
+++ mr-owlf-dss/mr_owlf_dss/main.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 
+from ingestors.reddit import Reddit
 from util import database as db
 from util.log import init
 
@@ -12,35 +13,25 @@ log = getLogger('root')
 
 
 def run(conn):
-    log.info(f'Posts: {conn.execute("SELECT COUNT(*) FROM posts").one()[0]}')
-    log.info(f'Inserting new post...')
-    conn.execute(
-        "INSERT INTO posts (author, title, content) VALUES (%s, %s, %s)",
-        ('dss', 'Post X', 'Content 0X')
-    )
 
-    log.info(f'(Updated) Posts: {conn.execute("SELECT COUNT(*) FROM posts").one()[0]}')
-    rows = conn.execute('SELECT * FROM posts')
-    for row in rows:
-        print(row.author, row.title, row.content)
+    posts = Reddit().exec()
+    print(f'{posts.head(2)}\n...\n{posts.tail(2)}')
+
+    # log.info(f'Posts: {conn.execute("SELECT COUNT(*) FROM posts").one()[0]}')
+    # log.info(f'Inserting new post...')
+    # conn.execute(
+    #     "INSERT INTO posts (author, title, content) VALUES (%s, %s, %s)",
+    #     ('dss', 'Post X', 'Content 0X')
+    # )
+    #
+    # log.info(f'(Updated) Posts: {conn.execute("SELECT COUNT(*) FROM posts").one()[0]}')
+    # rows = conn.execute('SELECT * FROM posts')
+    # for row in rows:
+    #     print(row.author, row.title, row.content)
 
 
 if __name__ == "__main__":
-    log.info("""
-          __________
-         / ___  ___ \\
-        / / @ \/ @ \ \\
-        \ \___/\___/ /\\
-         \____\/____/||
-         /     /\\\\\\\\\\//
-         |     |\\\\\\\\\\\\
-          \      \\\\\\\\\\\\
-           \______/\\\\\\\\
-            _||_||_
-             -- --
-            Mr. Owlf
-     > Data Stream Service <
-    """)
+    log.info('Starting Mr. Owlf: Data Stream Service...')
     _conn = db.connect()
     try:
         run(_conn)
