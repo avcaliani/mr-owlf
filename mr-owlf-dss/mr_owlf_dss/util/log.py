c602a904ec21e6ae64a980e08dbf1f9ebb1eee16
Anthony Caliani <avcaliani.it@gmail.com>
2020-01-02 11:17:15 -0300

    Updating log message

--- mr-owlf-dss/mr_owlf_dss/util/log.py
+++ mr-owlf-dss/mr_owlf_dss/util/log.py
@@ -15,7 +15,7 @@ def init() -> None:
     _console = StreamHandler()
     _console.setLevel(DEBUG)
     _console.setFormatter(Formatter(
-        fmt='\033[1;36;40m%(asctime)s \033[1;32;40m%(levelname)s:\033[0m %(message)s',
+        fmt='\033[1;36;40m[%(asctime)s]\033[1;32;40m[%(levelname)s]\033[0m %(message)s',
         datefmt='%y.%m.%d %H:%M:%S'
     ))
 
@@ -23,7 +23,7 @@ def init() -> None:
     _file = FileHandler(LOG_FILE, mode='w+', encoding='utf8')
     _file.setLevel(INFO)
     _file.setFormatter(Formatter(
-        fmt='%(asctime)s %(levelname)s: %(message)s',
+        fmt='[%(asctime)s][%(levelname)s] %(message)s',
         datefmt='%y.%m.%d %H:%M:%S'
     ))
 
