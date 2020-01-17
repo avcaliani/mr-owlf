549f064a4e2850a4d529c96ea0b741da7d119819
Anthony <avcaliani.it@gmail.com>
2020-01-16 21:34:18 -0300

    Code fix

--- mr-owlf-api/app/controllers/controller.py
+++ mr-owlf-api/app/controllers/controller.py
@@ -5,10 +5,10 @@ from flask import Blueprint, jsonify
 
 CLF_FILE = env.get('APP_CLF_FILE', '../.shared/classifier.pkl')
 VECTORIZER_FILE = env.get('APP_VECTORIZER_FILE', '../.shared/vectorizer.pkl')
-Controller = Blueprint('Controller', __name__, template_folder='templates')
+controller = Blueprint('Controller', __name__, template_folder='templates')
 
 
-@Controller.route('/find', methods=['GET'])
+@controller.route('/find', methods=['GET'])
 def get_tasks():
     file = open(CLF_FILE, 'rb')
     data = load(file)
