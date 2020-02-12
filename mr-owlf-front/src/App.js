8c45d09b6f3cfa6fd0e206811378eaae64ea76ac
Anthony <avcaliani.it@gmail.com>
2020-02-11 22:59:17 -0300

    Adding cool background

--- mr-owlf-front/src/App.js
+++ mr-owlf-front/src/App.js
@@ -47,7 +47,7 @@ function App() {
       </div>
       
       <div className="footer">
-        <b>Mr. Owlf</b> is made with <Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" /> in Brazil by <a href="https://github.com/avcaliani" target="_blank">@avcaliani</a>
+        <b>Mr. Owlf</b> is made with <Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" /> in Brazil by <a href="https://github.com/avcaliani" target="_blank" rel="noopener noreferrer">@avcaliani</a>
       </div>
     </div>
   );
