ccdd3110b1625beab9a29eabfc32870fcda4bfb7
Anthony <avcaliani.it@gmail.com>
2020-02-16 22:04:20 -0300

    Handling CORS

--- mr-owlf-api/app/main.py
+++ mr-owlf-api/app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, abort, request
+from flask_cors import CORS, cross_origin
 
 from service import statistic, score
 from util import log
@@ -8,16 +9,20 @@ __contact__ = 'https://github.com/avcaliani'
 __license__ = 'MIT'
 
 app = Flask(__name__, static_url_path='', static_folder='static')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 log.init()
 
 
 @app.route('/')
+@cross_origin()
 def home():
     return app.send_static_file('index.html')
 
 
 @app.route('/statistic', methods=['GET'])
-def get_tasks():
+@cross_origin()
+def get_statistics():
     result = statistic.find_last()
     if result is None:
         abort(503)
@@ -25,7 +30,8 @@ def get_tasks():
 
 
 @app.route('/score', methods=['POST'])
-def create_task():
+@cross_origin()
+def get_score():
     if request.json is None or 'sentence' not in request.json:
         abort(400)
     else:
@@ -35,4 +41,4 @@ def create_task():
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3000, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
